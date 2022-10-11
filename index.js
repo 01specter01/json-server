314f0a34818c9c8ade5a9c0b78197e6760cccfd8
Leo Volwahsen <volwahsenl@gmail.com>
2022-10-11 13:44:59 +0200

    g

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-fetch("http://localhost:4000/userdata");
+const displayData = async () => {
+    try {
+        const response = await fetch("http://localhost:4000/userdata");
+        if (response.status === 200) {
+            const result = await response.json();
+            console.log(result);
+        } else {
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+displayData();
