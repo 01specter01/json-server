3d14d22fce5c5e8d4dabbc53ca057abeb564de18
Leo Volwahsen <volwahsenl@gmail.com>
2022-10-12 09:03:50 +0200

    l

--- index.js
+++ index.js
@@ -3,12 +3,36 @@ const displayData = async () => {
         const response = await fetch("http://localhost:4000/userdata");
         if (response.status === 200) {
             const result = await response.json();
-            console.log(result);
+            peoplesData(result);
         } else {
+            throw new Error("Something whent wrong");
         }
     } catch (error) {
         console.log(error);
     }
 };
+const idElem = document.querySelectorAll(".id");
+
+const firstNameElem = document.querySelectorAll(".firstName");
 
 displayData();
+function peoplesData(result) {
+    idElem.forEach((element, index) => {
+        element.classList.add("idResult");
+
+        element.innerText = result[index].id;
+    });
+    console.log(result);
+    const dataInputElem = document.querySelectorAll("td");
+    console.log(dataInputElem);
+    dataInputElem.forEach((element, index) => {
+        element.classList.add("card");
+
+        idElem.innerText = result[index].idElem;
+        firstNameElem.innerText = result[index].firstNameElem;
+    });
+
+    // firstNameElem.forEach((element, index) => {
+    //     element.classList.add("firstNameResult");
+    // });
+}
